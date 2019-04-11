9e4a4cb0bb9e3fe643ea34644f08fb91607b6378
Daniel <dbaran7416@gmail.com>
2019-04-11 13:19:20 -0400

    Attempting functionality for the implementation for the camera

--- SelfTaught/app/src/main/java/com/example/self_taught/HomeFragmentFour.java
+++ SelfTaught/app/src/main/java/com/example/self_taught/HomeFragmentFour.java
@@ -1,17 +1,65 @@
 package com.example.self_taught;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CaptureRequest;
+import android.media.ImageReader;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.v4.app.Fragment;
+import android.util.Size;
+import android.util.SparseIntArray;
 import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.File;
 
 public class HomeFragmentFour extends Fragment {
     View view;
+
+    private static final String TAG = "Android Camera";
+    private Button takePicBtn;
+    private TextureView textureView;
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
+    private String cameraID;
+
+    protected CameraDevice camerDevice;
+    protected CameraCaptureSession cameraCaptureSessions;
+    protected CaptureRequest captureRequest;
+    protected CaptureRequest.Builder captureRequestBuilder;
+
+    private ImageReader imageReader;
+    private Size imageDimension;
+    private File file;
+    private static final int REQUEST_CAMERA_PERMISSION = 200;
+    private boolean mFlashSupported;
+    private Handler mBackgroundHandler;
+    private HandlerThread mBackgroundThread;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         view = inflater.inflate(R.layout.homemenufragfour, container, false);
         return view;
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        textureView = (TextureView) findViewById(R.id.HomeFragFourTextureView);
+    }
+
 }
