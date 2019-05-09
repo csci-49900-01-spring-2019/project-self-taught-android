d087c81b01350bd77a09e9bbf566704a7c9dea76
Daniel <dbaran7416@gmail.com>
2019-05-08 23:02:04 -0400

    added minor visual changes and taking picture options

--- SelfTaught/app/src/main/java/com/example/self_taught/Home.java
+++ SelfTaught/app/src/main/java/com/example/self_taught/Home.java
@@ -1,14 +1,25 @@
 package com.example.self_taught;
 
+import android.content.Intent;
+import android.graphics.Point;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
+
+    int x;
+    int y;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +29,34 @@ public class Home extends AppCompatActivity {
         TextView userText = (TextView) findViewById(R.id.HomeUsernameDsply);
         userText.setText(userName);
 
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        x = size.x;
+        y = size.y;
+
+        TextView homeMenuOne = (TextView) findViewById(R.id.HomeMenuOptOne);
+        TextView homeMenuTwo = (TextView) findViewById(R.id.HomeMenuOptTwo);
+        TextView homeMenuThree = (TextView) findViewById(R.id.HomeMenuOptThree);
+        TextView homeMenuFour = (TextView) findViewById(R.id.HomeMenuOptFour);
+        FrameLayout homeFrameLayout = (FrameLayout) findViewById(R.id.HomeFrameLayout);
+        ConstraintLayout homeConLayout = (ConstraintLayout) findViewById(R.id.HomeMenuConstraint);
+
+        homeConLayout.setMaxWidth(x);
+        homeConLayout.setMinWidth(x);
+        homeConLayout.setMaxHeight(y/2);
+        homeConLayout.setMinHeight(y/2);
+
+        Log.d("width", x + "");
+        Log.d("height", y +"");
+
+        //homeFrameLayout.setLayoutParams(new FrameLayout.LayoutParams(y, (x-(x/5))));
+
+
         //Starting menu for home
         loadFragment(new HomeFragmentOne());
 
         // First home page menu option
-        TextView homeMenuOne = (TextView) findViewById(R.id.HomeMenuOptOne);
         homeMenuOne.setOnClickListener(new View.OnClickListener()
         {
 
@@ -33,7 +67,6 @@ public class Home extends AppCompatActivity {
         });
 
         //Second home page menu option
-        TextView homeMenuTwo = (TextView) findViewById(R.id.HomeMenuOptTwo);
         homeMenuTwo.setOnClickListener(new View.OnClickListener()
         {
 
@@ -44,7 +77,6 @@ public class Home extends AppCompatActivity {
         });
 
         //Third home page menu option
-        TextView homeMenuThree = (TextView) findViewById(R.id.HomeMenuOptThree);
         homeMenuThree.setOnClickListener(new View.OnClickListener()
         {
 
@@ -55,7 +87,6 @@ public class Home extends AppCompatActivity {
         });
 
         //Fourth home page menu option
-        TextView homeMenuFour = (TextView) findViewById(R.id.HomeMenuOptFour);
         homeMenuFour.setOnClickListener(new View.OnClickListener()
         {
 
@@ -66,11 +97,21 @@ public class Home extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onBackPressed()
+    {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
+        finish();
+    }
+
     private void loadFragment(Fragment frag)
     {
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
-        fragmentTransaction.replace(R.id.homeFrameLayout, frag);
+        fragmentTransaction.replace(R.id.HomeFrameLayout, frag);
         fragmentTransaction.commit();
     }
 }
--- SelfTaught/app/src/main/java/com/example/self_taught/MainActivity.java
+++ SelfTaught/app/src/main/java/com/example/self_taught/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.self_taught;
 
 import android.content.Intent;
 import android.graphics.Point;
+import android.os.AsyncTask;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,10 +12,31 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Authenticator;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.HttpCookie;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.PasswordAuthentication;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class MainActivity extends AppCompatActivity {
     int x;
     int y;
-
+    URL obj;
+    HttpURLConnection conn;
+    CookieManager cManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +51,31 @@ public class MainActivity extends AppCompatActivity {
         y = size.y;
 
         //Different Views set beforehand
-        Button loginBtn = (Button) findViewById(R.id.loginBtn);
-        Button signUpBtn = (Button) findViewById(R.id.signUpBtn);
+        Button loginBtn = (Button) findViewById(R.id.LoginBtn);
+        Button signUpBtn = (Button) findViewById(R.id.SignUpBtn);
+
+        //Get all views
+        //Consider doing this all in a function
+        ConstraintLayout loginBox = (ConstraintLayout) findViewById(R.id.LoginBox);
+        TextView usernameText = (TextView) findViewById(R.id.UsernameText);
+        TextView passwordText = (TextView) findViewById(R.id.PasswordText);
+
+        //Set sizes (modular [is that the right word?])
+         loginBox.setMaxHeight(y/2);
+         loginBox.setMinHeight(y/2);
+         loginBox.setMaxWidth((3*x)/5);
+         loginBox.setMinWidth((3*x)/5);
+         usernameText.setHeight(y/16);
+         usernameText.setWidth((3*x)/5);
+         passwordText.setHeight(y/16);
+         passwordText.setWidth((3*x)/5);
+         loginBtn.setWidth((3*x)/10);
+         loginBtn.setHeight(y/16);
+        signUpBtn.setWidth((3*x)/10);
+        signUpBtn.setHeight(y/16);
+
+        //cManager = new CookieManager();
+       // CookieHandler.setDefault(cManager);
 
         //Login to get to the home page
         loginBtn.setOnClickListener(new View.OnClickListener()
@@ -38,13 +83,33 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                TextView userName = (TextView) findViewById(R.id.usernameText);
-                TextView passWord = (TextView) findViewById(R.id.passwordText);
-                String chckUser = userName.getText().toString();
-                String chckPass = passWord.getText().toString();
+                TextView userName = (TextView) findViewById(R.id.UsernameText);
+                TextView passWord = (TextView) findViewById(R.id.PasswordText);
+                final String chckUser = userName.getText().toString();
+                final String chckPass = passWord.getText().toString();
                 Log.d("login", "Username: " + chckUser + "\nPassword: " + chckPass);
 
                 //Api code to check if these match with the Database
+
+                String endPoint= "https://www.selftaughtapp.com/users/sign_in";
+                /**HttpCookie cookie = new HttpCookie("lang", "en");
+                cookie.setDomain("selftaught.com");
+                cookie.setPath("/users/sign_in");
+                cookie.setVersion(0);
+                try {
+                    cManager.getCookieStore().add(new URI("https://www.selftaughtapp.com/"), cookie);
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                }**/
+
+                //String endPoint = "http://google.com";
+
+                String urls[] = new String[3];
+                urls[0] = endPoint;
+                //urls[1] = "user[username]=" + chckUser + "&user[password]=" + chckPass;
+                urls[1] = "user[username]=testaccount&user[password]=password123";
+                urls[2] = String.valueOf(android.util.Base64.encode(urls[1].getBytes(), 0));
+                //new GetUrlContentTask().execute(urls);
 
                 Intent homeIntent = new Intent(getApplicationContext(), Home.class);
                 homeIntent.putExtra("username", chckUser);
@@ -64,15 +129,72 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        //Set sizes for layouts
-       /** ConstraintLayout loginBox = (ConstraintLayout) findViewById(R.id.LoginBox);
-        loginBox.setMaxHeight(y/2);
-        loginBox.setMinHeight(y/2);
-        loginBox.setMaxWidth((3*x)/5);
-        loginBox.setMinWidth((3*x)/5);
-        userName.setHeight(y/16);
-        userName.setWidth((3*x)/5);
-        passWord.setHeight(y/16);
-        passWord.setWidth((3*x)/5);**/
+
+    }
+}
+
+
+class GetUrlContentTask extends AsyncTask<String, Integer, String>
+{
+    protected String doInBackground(String... urls)
+    {
+        HttpsURLConnection connection;
+        String content = "", line;
+        String login = urls[1];
+        String encode = urls[2];
+
+        try {
+            URL url = new URL(urls[0]);
+            connection = (HttpsURLConnection) url.openConnection();
+            connection.setRequestProperty("Authorization", "Bearer " + encode);
+            connection.setRequestMethod("POST");
+            //connection.setDoOutput(true);
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            //For POST
+            OutputStream os = connection.getOutputStream();
+            BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            wr.write(login);
+            wr.flush();
+            wr.close();
+            os.close();
+
+            connection.connect();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            while((line = rd.readLine()) != null)
+            {
+                content += line + "\n";
+            }
+            connection.disconnect();
+
+            url = new URL("https://www.selftaughtapp.com/notebooks/new");
+            connection = (HttpsURLConnection) url.openConnection();
+            connection.setRequestProperty("Authorization", "Bearer " + encode);
+            connection.setRequestMethod("GET");
+            //connection.setDoOutput(true);
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            connection.connect();
+            rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            while((line = rd.readLine()) != null) {
+                content += line + "\n";
+            }
+            connection.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content;
+    }
+
+    protected void onProgressUpdate(Integer... progress)
+    {
+
+    }
+
+    protected void onPostExecute(String result)
+    {
+        Log.d("Result", result);
     }
 }
